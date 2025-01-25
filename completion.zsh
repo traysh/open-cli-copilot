d20e9ed06ab8e0f96fc81156e06182e27938ab06
Danilo Luvizotto <danilo.luvizotto@gmail.com>
2025-01-25 01:22:52 +0100

    Fix buffer parsing errors in completion.sh

--- completion.zsh
+++ completion.zsh
@@ -1,25 +1,31 @@
 #!/bin/zsh
 
+# Find the options file in the .tmp directory
 OPTIONS_FILE=$(find "${SCRIPT_DIR}/.tmp/" -name ".options_${pid}_??????" | head -n 1)
+
+# Check if OPTIONS_FILE is found
+if [[ -z "$OPTIONS_FILE" ]]; then
+    echo "Error: Options file not found."
+    return 1
+fi
+
 previous_suggestion=""
 
 # Define a custom strategy for zsh-autosuggest
 _zsh_autosuggest_strategy_my_custom_suggestion() {
-    # Temporary solution for the handling of special characters
-
-    # returns error if $BUFFER contains \ or [
-    typeset -g suggestion=$(grep -i "^$BUFFER" "$OPTIONS_FILE" | head -n 1)
-    
-    # returns error if $BUFFER contains ( or )
-    # typeset -g suggestion=$(grep -i "^$(_zsh_autosuggest_escape_command "$BUFFER")" "$OPTIONS_FILE" | head -n 1)
+    # Read the first line of the options file into the suggestion variable
+    typeset -g suggestion
+    read -r suggestion < "$OPTIONS_FILE"
 }
 
+# Update the post-display with the suggestion
 _update_postdisplay() {
     POSTDISPLAY="${suggestion#$BUFFER}"
     zle redisplay
 }
 zle -N _update_postdisplay_widget _update_postdisplay
 
+# Timer function to update suggestions periodically
 TRAPALRM() {
     _zsh_autosuggest_strategy_my_custom_suggestion
     if [[ -n "$suggestion" && "$suggestion" != "$previous_suggestion" ]]; then
@@ -27,7 +33,7 @@ TRAPALRM() {
         previous_suggestion="$suggestion"
     fi
 }
-# Set the timer interval to 1 second
+# Set the timer interval to 1 second (adjust as needed)
 TMOUT=1
 
 
